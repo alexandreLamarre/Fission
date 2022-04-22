3fd7507fd0f4ff010322b98d0fa139102706bf1d
alexandreLamarre <alex7285@gmail.com>
2022-04-22 15:40:00 -0400

    Refactor API module to its own folder

--- core/src/api/mod.rs
+++ core/src/api/mod.rs
@@ -1,7 +1,7 @@
 use axum::{routing::get, Router};
 use tokio;
 #[tokio::main]
-async fn main() {
+async fn setup_routes() {
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
     axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
