a5fcf48b472643d53b826d1f3bdf82bcc0b7de13
alexandreLamarre <alex7285@gmail.com>
2022-05-23 13:54:48 -0400

    Refactor example metric test into its own module

--- core/src/metrics.rs
+++ core/src/metrics.rs
@@ -21,22 +21,27 @@ impl Biz {
     }
 }
 
-pub fn test_biz() {
-    let biz = Arc::new(Biz::default());
-    let mut threads = Vec::new();
-    for _ in 0..5 {
-        let biz = Arc::clone(&biz);
-        let t = thread::spawn(move || {
-            for _ in 0..200 {
-                biz.biz();
-            }
-        });
-        threads.push(t);
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    pub fn test_biz() {
+        let biz = Arc::new(Biz::default());
+        let mut threads = Vec::new();
+        for _ in 0..5 {
+            let biz = Arc::clone(&biz);
+            let t = thread::spawn(move || {
+                for _ in 0..200 {
+                    biz.biz();
+                }
+            });
+            threads.push(t);
+        }
+        for t in threads {
+            t.join().unwrap();
+        }
+        // Print the results!
+        let serialized = serde_yaml::to_string(&*biz).unwrap();
+        println!("{}", serialized);
     }
-    for t in threads {
-        t.join().unwrap();
-    }
-    // Print the results!
-    let serialized = serde_yaml::to_string(&*biz).unwrap();
-    println!("{}", serialized);
 }
