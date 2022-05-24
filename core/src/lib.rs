2b94dc4677bc02e7e7c6a1590264d08f8a571953
alexandreLamarre <alex7285@gmail.com>
2022-05-24 09:33:39 -0400

    Setup a consistent logging framework for Fission

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,8 +1,40 @@
 #![warn(dead_code)]
 
+extern crate fern;
 extern crate threadpool;
+
+use log::{debug, error, info, trace, warn};
+
 mod api;
 mod dataset;
 mod metrics;
 mod node;
 mod reader;
+
+/// @param file_name : name of the file, will be suffixed with .log
+pub fn setup_logger(file_name: &str) -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .chain(fern::log_file(format!("{}.log", file_name))?)
+        .apply()?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works_setup_logger() {
+        assert!(setup_logger("lib-test.log").is_ok());
+    }
+}
--- core/src/main.rs
+++ core/src/main.rs
@@ -1,8 +1,18 @@
 #![warn(dead_code)]
 mod metrics;
 
-use crate::metrics::*;
+use core::setup_logger;
 
 fn main() {
     println!("Running Fission ....");
+    println!("Setting up logger ...");
+    match setup_logger("main") {
+        Ok(_) => {
+            println!("Logger setup successfully");
+        }
+        Err(e) => {
+            panic!("Error setting up logger: {}", e);
+        }
+    }
+    println!("... Fission finished!");
 }
